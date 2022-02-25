dbbfed91c296e0b4e874c4eaf0ee97b0da43227a
JIMO_17_ <jies1702@gmail.com>
2022-02-25 10:10:54 -0500

    project setup

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
+const { typeDefs } = require('./typeDefs');
+const { resolvers } = require('./resolvers');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Welcome to my api'))
+
+async function start(){
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers
+    })
+    
+    await apolloServer.start()
+    
+    apolloServer.applyMiddleware({app})
+    
+    app.get('*', (req, res) => res.status(404).send('Not found'))
+
+    app.listen(3000, ()=> {
+        console.log(`Server on port`, 3000)
+    })
+}
+
+start();
---
+++ resolvers.js
@@ -0,0 +1,7 @@
+const resolvers = {
+    Query: {
+        hello: () => 'Hello Word'
+    }
+}
+
+module.exports = { resolvers };
